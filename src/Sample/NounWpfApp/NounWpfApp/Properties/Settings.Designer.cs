0f5d5923d4412b2697c764af7fd72f173b3ca408
Yavor <yavor87@gmail.com>
2018-01-02 16:17:58 +0200

    Listing icons

--- src/Sample/NounWpfApp/NounWpfApp/Properties/Settings.Designer.cs
+++ src/Sample/NounWpfApp/NounWpfApp/Properties/Settings.Designer.cs
@@ -8,22 +8,42 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NounWpfApp.Properties
-{
-
-
+namespace NounWpfApp.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string AppKey {
+            get {
+                return ((string)(this["AppKey"]));
+            }
+            set {
+                this["AppKey"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string AppSecret {
+            get {
+                return ((string)(this["AppSecret"]));
+            }
+            set {
+                this["AppSecret"] = value;
             }
         }
     }
--- src/Sample/NounWpfApp/NounWpfApp/ViewModel/MainViewModel.cs
+++ src/Sample/NounWpfApp/NounWpfApp/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using NounSharp;
+using NounSharp.Models;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows.Input;
 
@@ -10,30 +12,53 @@ namespace NounWpfApp.ViewModel
         public MainViewModel()
         {
             _searchCommand = new DelegateCommand(OnSearch);
+            this.Key = Properties.Settings.Default.AppKey;
+            this.Secret = Properties.Settings.Default.AppSecret;
         }
 
         private string _key, _secret;
         private ICommand _searchCommand;
-
-        public string Key { get => _key; set => SetProperty(ref _key, value); }
-        public string Secret { get => _secret; set => SetProperty(ref _secret, value); }
-        public ICommand SearchCommand => _searchCommand;
+        private IEnumerable<Icon> _icons;
+        private NounProjectService _service;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private void SetProperty<T>(ref T storage, T value, [System.Runtime.CompilerServices.CallerMemberName]string propertyName = "")
+        public string Key { get => _key; set => SetProperty(ref _key, value, changedHandler: OnKeyChanged); }
+        public string Secret { get => _secret; set => SetProperty(ref _secret, value, changedHandler: OnSecretChanged); }
+        public IEnumerable<Icon> Icons { get => _icons; set => SetProperty(ref _icons, value); }
+        public ICommand SearchCommand => _searchCommand;
+
+        private bool SetProperty<T>(ref T storage, T value, [System.Runtime.CompilerServices.CallerMemberName]string propertyName = "", Action<T, T> changedHandler = null)
         {
             if (object.Equals(storage, value))
-                return;
+                return false;
 
+            T old = storage;
             storage = value;
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            changedHandler?.Invoke(old, value);
+            return true;
         }
 
         private async void OnSearch(object parameter)
         {
-            NounProjectService service = new NounProjectService(this.Key, this.Secret);
-            var icons = await service.GetIconsAsync(parameter?.ToString());
+            if (_service == null)
+            {
+                _service = new NounProjectService(this.Key, this.Secret);
+            }
+            Icons = await _service.GetIconsAsync(parameter?.ToString());
+        }
+
+        private void OnKeyChanged(string old, string newValue)
+        {
+            Properties.Settings.Default.AppKey = newValue;
+            Properties.Settings.Default.Save();
+        }
+
+        private void OnSecretChanged(string old, string newValue)
+        {
+            Properties.Settings.Default.AppSecret = newValue;
+            Properties.Settings.Default.Save();
         }
     }
 
