1fa4071a02f9afe6b6b0af631ffdb719869623a3
Yavor <yavor87@gmail.com>
2018-01-08 17:06:27 +0200

    Adding icon selection

--- src/Sample/NounWpfApp/NounWpfApp/ViewModel/MainViewModel.cs
+++ src/Sample/NounWpfApp/NounWpfApp/ViewModel/MainViewModel.cs
@@ -12,13 +12,15 @@ namespace NounWpfApp.ViewModel
         public MainViewModel()
         {
             _searchCommand = new DelegateCommand(OnSearch);
+            _openLinkCommand = new DelegateCommand(OnOpenLink);
             this.Key = Properties.Settings.Default.AppKey;
             this.Secret = Properties.Settings.Default.AppSecret;
         }
 
         private string _key, _secret;
-        private ICommand _searchCommand;
+        private ICommand _searchCommand, _openLinkCommand;
         private IEnumerable<Icon> _icons;
+        private bool _publicDomainOnly;
         private NounProjectService _service;
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -26,7 +28,9 @@ namespace NounWpfApp.ViewModel
         public string Key { get => _key; set => SetProperty(ref _key, value, changedHandler: OnKeyChanged); }
         public string Secret { get => _secret; set => SetProperty(ref _secret, value, changedHandler: OnSecretChanged); }
         public IEnumerable<Icon> Icons { get => _icons; set => SetProperty(ref _icons, value); }
+        public bool PublicDomainOnly { get => _publicDomainOnly; set => SetProperty(ref _publicDomainOnly, value); }
         public ICommand SearchCommand => _searchCommand;
+        public ICommand OpenLinkCommand => _openLinkCommand;
 
         private bool SetProperty<T>(ref T storage, T value, [System.Runtime.CompilerServices.CallerMemberName]string propertyName = "", Action<T, T> changedHandler = null)
         {
@@ -46,7 +50,15 @@ namespace NounWpfApp.ViewModel
             {
                 _service = new NounProjectService(this.Key, this.Secret);
             }
-            Icons = await _service.GetIconsAsync(parameter?.ToString());
+            Icons = await _service.GetIconsAsync(parameter?.ToString(), limitToPublicDomain: _publicDomainOnly);
+        }
+
+        private void OnOpenLink(object obj)
+        {
+            Uri urlToNavigate = obj as Uri;
+            if (urlToNavigate == null)
+                return;
+            System.Diagnostics.Process.Start(urlToNavigate.AbsoluteUri);
         }
 
         private void OnKeyChanged(string old, string newValue)
